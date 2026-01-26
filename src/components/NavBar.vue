882dafce0287ca3ce785ef4f8db4c87ed2506aee
Sasha <sasha342008@gmail.com>
2026-01-26 20:46:48 +0200

    update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -23,7 +23,7 @@ const menuOpen = ref(false);
 
     <div v-if="menuOpen" class="menu">
       <a class="menu-link" @click="$router.push('/cats')">🐱 Коти</a>
-      <a href="#">🐶 Собаки</a>
+      <a class="menu-link" @click="$router.push('/dogs')">🐶 Собаки</a>
       <a href="#">🐹 Хом'яки</a>
       <a href="#">🦜 Папугаї</a>
     </div>
